41190921bb182bc1ea733733da8fc3dc302ff429
Mateusz Wojczal <mateusz@wojczal.com>
2022-04-22 13:50:30 +0200

    check my types

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const dodawanie = (x: number, y: number): number => "ssss";
+const dodawanie = (x: number, y: number): number => x + y;
 
 function App() {
   return (
