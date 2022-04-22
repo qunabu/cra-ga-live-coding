fe74f11488e2525217248a99529ecd2595188639
Mateusz Wojczal <mateusz@wojczal.com>
2022-04-22 13:54:54 +0200

    jestem hardcorem

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-const dodawanie = (x: number, y: number): number => x + y;
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Witaj swiecie s</h1>
+        <h1>Czesc Escolaki</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
