2383e18b0b58648017ab072b8794848104a0ccdb
Mateusz Qunabu <mateusz@qunabu.com>
2022-04-22 13:51:25 +0200

    errror (#1)
    * errror
    
    * errror fixed
    
    * check my types
    
    * check my types
    
    * check my types
    
    Co-authored-by: Mateusz Wojczal <mateusz@wojczal.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const dodawanie = (x: number, y: number): number => x + y;
+
 function App() {
   return (
     <div className="App">
