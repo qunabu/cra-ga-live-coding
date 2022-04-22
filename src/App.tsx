fe591918739a5b9625098671acbaa036fa12d168
Mateusz Wojczal <mateusz@wojczal.com>
2022-04-22 13:33:15 +0200

    tests

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Witaj swiecie</h1>
+        <h1>Witaj swiecie s</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
